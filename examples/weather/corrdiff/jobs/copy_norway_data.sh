ba143a18fcd0c15441f387f42465ef01fbfdb443
shiemn <simon.hentschel@uni-wuerzburg.de>
2025-10-13 20:18:23 +0200

    add some configs and slurm scripts

--- examples/weather/corrdiff/jobs/copy_norway_data.sh
+++ examples/weather/corrdiff/jobs/copy_norway_data.sh
@@ -1,10 +1,10 @@
 echo "Copying HCLIM3 to scratch"
 #measure runtime
 start=$(date +%s.%N)
-mkdir -p /scratch/Norway/HCLIM3/preprocessed/
+mkdir -p /scratch/Norway/HCLIM3/preprocessed_large/
 
 
-cp -n -r /anvme/workspace/b214cb13-ecodata/downscaling/Norway/HCLIM3/preprocessed/NorCP_AROME_EC-EARTH /scratch/Norway/HCLIM3/preprocessed/NorCP_AROME_EC-EARTH &
+cp -n -r /anvme/workspace/b214cb13-ecodata/downscaling/Norway/HCLIM3/preprocessed_large/NorCP_AROME_EC-EARTH /scratch/Norway/HCLIM3/preprocessed_large/NorCP_AROME_EC-EARTH &
 cp /anvme/workspace/b214cb13-ecodata/downscaling/Norway/HCLIM3/orog_NEU-3_ECMWF-ERAINT_evaluation_r1i1p1_HCLIMcom-HCLIM38-AROME_x2yn2v1_fx.nc /scratch/Norway/HCLIM3/.
 
 wait
