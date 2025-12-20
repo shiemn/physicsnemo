e4cd4ffb1447adcc088f2f6c3823aca461d38b78
shiemn <simon.hentschel@uni-wuerzburg.de>
2025-12-20 17:50:01 +0100

    Add new config files, restructure layout of slurm/conf files

---
+++ examples/weather/corrdiff/jobs/helma/copy.sh
@@ -0,0 +1,15 @@
+echo "Copying HCLIM3 to scratch"
+#measure runtime
+start=$(date +%s.%N)
+mkdir -p /scratch/Norway/HCLIM3/preprocessed_large/
+
+
+cp -n -r /hnvme/workspace/b214cb11-helma-ecodata/downscaling/Norway/HCLIM3/preprocessed_large/NorCP_AROME_EC-EARTH /scratch/Norway/HCLIM3/preprocessed_large/NorCP_AROME_EC-EARTH &
+cp /hnvme/workspace/b214cb11-helma-ecodata/downscaling/Norway/HCLIM3/orog_NEU-3_ECMWF-ERAINT_evaluation_r1i1p1_HCLIMcom-HCLIM38-AROME_x2yn2v1_fx.nc /scratch/Norway/HCLIM3/.
+
+wait
+
+
+end=$(date +%s.%N)
+echo "Copying data to scratch took $(awk "BEGIN {print ${end} - ${start}}") seconds"
+echo "Copying data to scratch complete"
